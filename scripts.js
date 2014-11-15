4f68d292218870c3488534a5aba7570760e4ebd9
jaxuru <jaxreiff@gmail.com>
2014-11-15 15:55:40 -0500

    removed comment

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
- 
+
 $(document).ready(function f() {
 
 	$(window).on("load resize scroll",function(e){
@@ -77,45 +77,5 @@ $(document).ready(function f() {
 				f.turn = !f.turn;
 			}
 		}
-
-		// var blackcheck = $(".tile.black");
-		// var whitecheck = $(".tile.white");
-
-		// for(i = 0; i < blackcheck.length; i++) {	//Capture of Black Pieces
-
-		// 	var $check = $("#"+blackcheck[i].id);
-
-		// 	var tid = parseInt(($check.attr('id')).slice(1));
-		// 	var up = (tid - 7).toString();
-  //       	var down = (tid + 7).toString();
-  //       	var left = (tid - 1).toString();
-		// 	var right = (tid + 1).toString();
-
-		// 	if (($("#t" + up).hasClass("white") || $("#t" + up).length == 0)
-		// 		&& ($("#t"+down).hasClass("white") || $("#t" + down).length == 0)
-		// 		&& ($("#t"+left).hasClass("white") || left % 7 == 0)
-		// 		&& ($("#t"+right).hasClass("white") || right % 7 == 1))
-		// 	{
-		// 		$check.removeClass("black");
-		// 	}
-		// }
-		// for(i = 0; i < whitecheck.length; i++) {	//Capture of White Pieces
-
-		// 	var $check = $("#"+whitecheck[i].id);
-
-		// 	var tid = parseInt(($check.attr('id')).slice(1));
-		// 	var up = (tid - 7).toString();
-  //       	var down = (tid + 7).toString();
-  //       	var left = (tid - 1).toString();
-		// 	var right = (tid + 1).toString();
-
-		// 	if (($("#t" + up).hasClass("black") || $("#t" + up).length == 0)
-		// 		&& ($("#t"+down).hasClass("black") || $("#t" + down).length == 0)
-		// 		&& ($("#t"+left).hasClass("black") || left % 7 == 0)
-		// 		&& ($("#t"+right).hasClass("black") || right % 7 == 1))
-		// 	{
-		// 		$check.removeClass("white");
-		// 	}
-		// }
 	});
 });
