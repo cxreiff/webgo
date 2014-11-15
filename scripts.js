4b58834d3819ea2acd55fbff20a483a1eb2232cb
cxreiff <cooper@cxreiff.com>
2014-11-15 15:53:39 -0500

    capturing on edges now functioning

--- scripts.js
+++ scripts.js
@@ -21,6 +21,28 @@ $(document).ready(function f() {
 				$(".marker").removeClass("black");
 				$(".marker").addClass("white");
 
+				
+				var whitecheck = $(".tile.white");
+
+				for(i = 0; i < whitecheck.length; i++) {	//Capture of White Pieces
+
+					var $check = $("#"+whitecheck[i].id);
+
+					var tid = parseInt(($check.attr('id')).slice(1));
+					var up = (tid - 7).toString();
+		        	var down = (tid + 7).toString();
+		        	var left = (tid - 1).toString();
+					var right = (tid + 1).toString();
+
+					if (($("#t" + up).hasClass("black") || $("#t" + up).length == 0)
+						&& ($("#t"+down).hasClass("black") || $("#t" + down).length == 0)
+						&& ($("#t"+left).hasClass("black") || left % 7 == 0)
+						&& ($("#t"+right).hasClass("black") || right % 7 == 1))
+					{
+						$check.removeClass("white");
+					}
+				}
+
 				f.turn = !f.turn;
 			}
 			else
@@ -30,42 +52,70 @@ $(document).ready(function f() {
 				$(".marker").removeClass("white");
 				$(".marker").addClass("black");
 
+
+				var blackcheck = $(".tile.black");
+
+				for(i = 0; i < blackcheck.length; i++) {	//Capture of Black Pieces
+
+					var $check = $("#"+blackcheck[i].id);
+
+					var tid = parseInt(($check.attr('id')).slice(1));
+					var up = (tid - 7).toString();
+		        	var down = (tid + 7).toString();
+		        	var left = (tid - 1).toString();
+					var right = (tid + 1).toString();
+
+					if (($("#t" + up).hasClass("white") || $("#t" + up).length == 0)
+						&& ($("#t"+down).hasClass("white") || $("#t" + down).length == 0)
+						&& ($("#t"+left).hasClass("white") || left % 7 == 0)
+						&& ($("#t"+right).hasClass("white") || right % 7 == 1))
+					{
+						$check.removeClass("black");
+					}
+				}
+
 				f.turn = !f.turn;
 			}
 		}
 
-		var blackcheck = $(".tile.black");
-		var whitecheck = $(".tile.white");
+		// var blackcheck = $(".tile.black");
+		// var whitecheck = $(".tile.white");
 
-		for(i = 0; i < blackcheck.length; i++) {	//Capture of Black Pieces
+		// for(i = 0; i < blackcheck.length; i++) {	//Capture of Black Pieces
 
-			var $check = $("#"+blackcheck[i].id);
+		// 	var $check = $("#"+blackcheck[i].id);
 
-			var tid = parseInt(($check.attr('id')).slice(1));
-			var up = (tid - 7).toString();
-        	var down = (tid + 7).toString();
-        	var left = (tid - 1).toString();
-			var right = (tid + 1).toString();
+		// 	var tid = parseInt(($check.attr('id')).slice(1));
+		// 	var up = (tid - 7).toString();
+  //       	var down = (tid + 7).toString();
+  //       	var left = (tid - 1).toString();
+		// 	var right = (tid + 1).toString();
 
-			if ($("#t" + up).hasClass("white") && $("#t"+down).hasClass("white") && $("#t"+left).hasClass("white") && $("#t"+right).hasClass("white"))
-			{
-				$check.removeClass("black");
-			}
-		}
-		for(i = 0; i < whitecheck.length; i++) {	//Capture of White Pieces
+		// 	if (($("#t" + up).hasClass("white") || $("#t" + up).length == 0)
+		// 		&& ($("#t"+down).hasClass("white") || $("#t" + down).length == 0)
+		// 		&& ($("#t"+left).hasClass("white") || left % 7 == 0)
+		// 		&& ($("#t"+right).hasClass("white") || right % 7 == 1))
+		// 	{
+		// 		$check.removeClass("black");
+		// 	}
+		// }
+		// for(i = 0; i < whitecheck.length; i++) {	//Capture of White Pieces
 
-			var $check = $("#"+whitecheck[i].id);
+		// 	var $check = $("#"+whitecheck[i].id);
 
-			var tid = parseInt(($check.attr('id')).slice(1));
-			var up = (tid - 7).toString();
-        	var down = (tid + 7).toString();
-        	var left = (tid - 1).toString();
-			var right = (tid + 1).toString();
+		// 	var tid = parseInt(($check.attr('id')).slice(1));
+		// 	var up = (tid - 7).toString();
+  //       	var down = (tid + 7).toString();
+  //       	var left = (tid - 1).toString();
+		// 	var right = (tid + 1).toString();
 
-			if ($("#t" + up).hasClass("black") && $("#t"+down).hasClass("black") && $("#t"+left).hasClass("black") && $("#t"+right).hasClass("black"))
-			{
-				$check.removeClass("white");
-			}
-		}
+		// 	if (($("#t" + up).hasClass("black") || $("#t" + up).length == 0)
+		// 		&& ($("#t"+down).hasClass("black") || $("#t" + down).length == 0)
+		// 		&& ($("#t"+left).hasClass("black") || left % 7 == 0)
+		// 		&& ($("#t"+right).hasClass("black") || right % 7 == 1))
+		// 	{
+		// 		$check.removeClass("white");
+		// 	}
+		// }
 	});
 });
