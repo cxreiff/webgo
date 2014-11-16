d21ba9c02a6cd77f89b5b8474d8eb363aa067116
jaxuru <jaxreiff@gmail.com>
2014-11-16 18:43:08 -0500

    moved capture checking to functions, added outlining comment

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
 
+//Capture Checking Strategy:	If neighbors of checked stone are opposite color, capture.
+//								If one neighbor is empty, don't capture.
+//								For each neighbor that's the same color, check that neighbor 
+//								for capture not including the space starting the recursive call.
+//								If no piece in the recursive stack finds an empty spot, capture all.
+//								(Keep list of tiles checked recursively so that all can be either
+//								captured or removed from the list of pieces to check, and to prevent
+//								loops where a recursively checked tile checks its caller.)
+
 $(document).ready(function f() {
 
-	$(window).on("load resize scroll",function(e){
+	$(window).on("load resize scroll",function(e){	//Keeps board square when width is dynamically set.
     	var cw = $('.board').width();
 		$('.board').css({
     		'height': cw + 'px'
@@ -21,27 +30,7 @@ $(document).ready(function f() {
 				$(".marker").removeClass("black");
 				$(".marker").addClass("white");
 
-				
-				var whitecheck = $(".tile.white");
-
-				for(i = 0; i < whitecheck.length; i++) {	//Capture of White Pieces
-
-					var $check = $("#"+whitecheck[i].id);
-
-					var tid = parseInt(($check.attr('id')).slice(1));
-					var up = (tid - 7).toString();
-		        	var down = (tid + 7).toString();
-		        	var left = (tid - 1).toString();
-					var right = (tid + 1).toString();
-
-					if (($("#t" + up).hasClass("black") || $("#t" + up).length == 0)
-						&& ($("#t"+down).hasClass("black") || $("#t" + down).length == 0)
-						&& ($("#t"+left).hasClass("black") || left % 7 == 0)
-						&& ($("#t"+right).hasClass("black") || right % 7 == 1))
-					{
-						$check.removeClass("white");
-					}
-				}
+				captureWhite();
 
 				f.turn = !f.turn;
 			}
@@ -52,30 +41,58 @@ $(document).ready(function f() {
 				$(".marker").removeClass("white");
 				$(".marker").addClass("black");
 
-
-				var blackcheck = $(".tile.black");
-
-				for(i = 0; i < blackcheck.length; i++) {	//Capture of Black Pieces
-
-					var $check = $("#"+blackcheck[i].id);
-
-					var tid = parseInt(($check.attr('id')).slice(1));
-					var up = (tid - 7).toString();
-		        	var down = (tid + 7).toString();
-		        	var left = (tid - 1).toString();
-					var right = (tid + 1).toString();
-
-					if (($("#t" + up).hasClass("white") || $("#t" + up).length == 0)
-						&& ($("#t"+down).hasClass("white") || $("#t" + down).length == 0)
-						&& ($("#t"+left).hasClass("white") || left % 7 == 0)
-						&& ($("#t"+right).hasClass("white") || right % 7 == 1))
-					{
-						$check.removeClass("black");
-					}
-				}
+				captureBlack();
 
 				f.turn = !f.turn;
 			}
 		}
 	});
 });
+
+function captureWhite() {
+
+	var whitecheck = $(".tile.white");
+
+	for(i = 0; i < whitecheck.length; i++) {
+
+		var $check = $("#"+whitecheck[i].id);
+
+		var tid = parseInt(($check.attr('id')).slice(1));
+		var up = (tid - 7).toString();
+    	var down = (tid + 7).toString();
+    	var left = (tid - 1).toString();
+		var right = (tid + 1).toString();
+
+		if (($("#t" + up).hasClass("black") || $("#t" + up).length == 0)
+			&& ($("#t"+down).hasClass("black") || $("#t" + down).length == 0)
+			&& ($("#t"+left).hasClass("black") || left % 7 == 0)
+			&& ($("#t"+right).hasClass("black") || right % 7 == 1))
+		{
+			$check.removeClass("white");
+		}
+	}
+}
+
+function captureBlack() {
+
+	var blackcheck = $(".tile.black");
+
+	for(i = 0; i < blackcheck.length; i++) {
+
+		var $check = $("#"+blackcheck[i].id);
+
+		var tid = parseInt(($check.attr('id')).slice(1));
+		var up = (tid - 7).toString();
+    	var down = (tid + 7).toString();
+    	var left = (tid - 1).toString();
+		var right = (tid + 1).toString();
+
+		if (($("#t" + up).hasClass("white") || $("#t" + up).length == 0)
+			&& ($("#t"+down).hasClass("white") || $("#t" + down).length == 0)
+			&& ($("#t"+left).hasClass("white") || left % 7 == 0)
+			&& ($("#t"+right).hasClass("white") || right % 7 == 1))
+		{
+			$check.removeClass("black");
+		}
+	}
+}
