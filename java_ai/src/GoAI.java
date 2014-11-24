d1bf3dd6c7db84572762170c0d8e6ab5c0a2e7f3
cxreiff <cooper@cxreiff.com>
2014-11-24 14:48:46 -0500

    modified .gitignore, added java_ai

---
+++ java_ai/src/GoAI.java
@@ -0,0 +1,14 @@
+/**
+ * COSC 331 - Programming Project - jreiff17
+ *
+ * Java AI for Web Go game. Decides computer's moves and analyzes endgame positions.
+ */
+
+public class GoAI
+{
+
+    public static void main(String[] args)
+    {
+
+    }
+}
