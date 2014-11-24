7113f013afb323f60db7c71ec244be65a68091dc
jaxuru <jaxreiff@gmail.com>
2014-11-24 16:38:23 -0500

    modified .gitignore, modified GoAI

--- java_ai/src/GoAI.java
+++ java_ai/src/GoAI.java
@@ -7,8 +7,28 @@
 public class GoAI
 {
 
-    public static void main(String[] args)
+    public static String main(String[] args)
     {
+        String input = new String("0xxx");    //input string representation of game board state.
 
+        int op = Integer.parseInt(input.substring(0,1));
+        String pos = input.substring(1);
+
+        String result;
+
+        if(op==0) result = nextMove(pos);
+        else result = evalEnd(pos);
+
+        return result;
+    }
+
+    public static String evalEnd(String pos)
+    {
+        return pos;
+    }
+
+    public static String nextMove(String pos)
+    {
+        return "yyy";
     }
 }
