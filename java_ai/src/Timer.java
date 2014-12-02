531af3aa6914546b91c209d4b6d3941ba82b11b9
cxreiff <cooper@cxreiff.com>
2014-12-02 00:55:11 -0500

    added Timer

---
+++ java_ai/src/Timer.java
@@ -0,0 +1,32 @@
+/**
+ * COSC 331 - Programming Project - jreiff17
+ *
+ * Timer to time amount of searching before deciding on a move.
+ */
+
+public class Timer
+{
+
+	long start;
+
+	public Timer()
+	{
+		start = System.currentTimeMillis();
+	}
+
+	public long elapsed()
+	{
+		return System.currentTimeMillis() - start;
+	}
+
+	public void reset()
+	{
+		start = System.currentTimeMillis();
+	}
+
+	public String toString()
+	{
+		return elapsed() + "ms elapsed";
+	}
+
+}
