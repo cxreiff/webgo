f36805269f3ce44cf4df3971ac6166868f1585cc
cxreiff <cooper@cxreiff.com>
2014-12-04 17:25:32 -0500

    added comments, getting input

--- java_ai/src/GoAI.java
+++ java_ai/src/GoAI.java
@@ -1,7 +1,9 @@
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.ArrayList;
 
 /**
@@ -9,7 +11,7 @@ import java.util.ArrayList;
  *
  * Java AI for Web Go game. Decides computer's moves and analyzes endgame positions.
  *
- * Board states are encoded in the form PTxSSSSSS...S, representing the following:
+ * Board states are encoded in the form PTxSSSSSS(...)SSSxKKKKK(...)K, representing the following:
  *
  * 		P :	Respresents the type of information being asked for. Is "0" when asking
  * 			for the next move, and "1" when asking for a count of surrounded territory.
@@ -24,13 +26,45 @@ import java.util.ArrayList;
  * 			characters as board tiles, and are ordered from the top left to bottom right in
  * 			left to right, top to bottom order (english reading order).
  *
+ * 		K :	Represents the state of a given tile for a state in the list of recent board
+ * 			positions, collected in order to enforce the rule of ko (you cannot return the
+ * 			board to a previous position. There are six times as many K characters as S
+ * 			characters, to fill the six most recent previous board states.
+ *
  * 	This class handles http requests from the game running with javascript on an html web page.
  */
 public class GoAI
 {
 	public static void main(String[] args)
 	{
+		int port = 8080;
 
+		if(args.length > 0)
+		{
+			try
+			{
+				port = Integer.parseInt(args[0]);
+			}
+			catch(NumberFormatException ex)
+			{
+				System.err.println("USAGE: java YahtzeeService [port]");
+				System.exit(1);
+			}
+		}
+
+		try
+		{
+			InetSocketAddress addr = new InetSocketAddress(port);
+			HttpServer server = HttpServer.create(addr, 1);
+
+			server.createContext("/webgo/go.html", new GoHandler());
+
+			server.start();
+		}
+		catch(IOException ex)
+		{
+			ex.printStackTrace();
+		}
 	}
 
 	public static class GoHandler implements HttpHandler
@@ -43,20 +77,11 @@ public class GoAI
 		static Timer timer;
 
 		@Override
-		public void handle(HttpExchange httpExchange) throws IOException
+		public void handle(HttpExchange ex) throws IOException
 		{
-			//TODO Take input from javascript go page.
 
 			//input string representation of game board state.
-			String input = "00x"+"bweewwwbwwwwwwbwwwwwwbwwwwwwbwwwwwwbwwwwwwebbbbbb";
-
-			kolist = new String[6];
-			kolist[0] = "ebebewewbwebewbwebebwwwbeeeeebbebebwbbbwebeeewbb1";
-			kolist[1] = "ebebewewbwebewbwebebwwwbeeeeebbebebwbbbwebeeewbb2";
-			kolist[2] = "ebebewewbwebewbwebebwwwbeeeeebbebebwbbbwebeeewbb3";
-			kolist[3] = "bwebwwwbwwwwwwbwwwwwwbwwwwwwbwwwwwwbwwwwwwebbbbbb";
-			kolist[4] = "ebebewewbwebewbwebebwwwbeeeeebbebebwbbbwebeeewbb5";
-			kolist[5] = "ebebewewbwebewbwebebwwwbeeeeebbebebwbbbwebeeewbb6";
+			String input = ex.getRequestURI().getQuery();
 
 			//Determines whether the AI is being asked for 0: the best next move, or 1: an endgame evaluation.
 			int op = Integer.parseInt(input.substring(0, 1));
@@ -64,14 +89,27 @@ public class GoAI
 			//Determines whether 0: black plays next or 1: white plays next.
 			turn = Integer.parseInt(input.substring(1, 2));
 
-			//string representation of the board and its stones.
-			String pos = input.substring(3);
+			int split = 52;
+			for(int i = 3; i < input.length(); i++)
+			{
+				if(input.charAt(i) == 'x') split = i;
+			}
 
-			//Timer to determine how long to "think" before making a move.
-			timer = new Timer();
+			//string representation of the board and its stones.
+			String pos = input.substring(3, split);
 
 			//Length of each side of the board.
 			n = (int)(Math.sqrt((pos.length())));
+
+			//Populate kolist.
+			kolist = new String[6];
+			for(int i = 0; i < 6; i++)
+			{
+				kolist[i] = input.substring(split+1+(n*n*i), split+1+(n*n*(i+1)));
+			}
+
+			//Timer to determine how long to "think" before making a move.
+			timer = new Timer();
 
 			//2D matrix representation of input board, 0: empty, 1: black, 2: white.
 			printBoard(genBoard(pos));
@@ -80,9 +118,6 @@ public class GoAI
 			System.out.println(((turn == 0)?"black":"white")+" turn"+"\n");
 
 			String result = (op == 0) ? nextMove(pos, turn) : evalEnd(pos);
-
-			System.out.println("R: "+result);
-			System.out.println();
 
 			//2D matrix representation of output board, 0: empty, 1: black, 2: white.
 			printBoard(genBoard(result));
@@ -115,8 +150,6 @@ public class GoAI
 				//Use evaluated random play from that new node to evaluate it.
 				current.evaluate();
 			}
-
-			System.out.println("V: "+root.bestChild().getValue());
 
 			//When all node adjustments have been made and time has run out, choose the depth:1 option with the best value.
 			return root.bestChild().getPos();
